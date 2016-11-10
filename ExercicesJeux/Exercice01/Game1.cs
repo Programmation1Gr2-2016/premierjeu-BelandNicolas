061be3586f250bc7b4ae435f2462a44110961346
BelandNicolas <beland.nicolas@carrefour.cegepvicto.ca>
2016-11-10 17:03:46 -0500

    AddEnemyFireBall

--- ExercicesJeux/Exercice01/Game1.cs
+++ ExercicesJeux/Exercice01/Game1.cs
@@ -9,10 +9,14 @@ namespace Exercice01
     /// </summary>
     public class Game1 : Game
     {
+        bool isDirection = false;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Rectangle fenetre;
         GameObject heros;
+        GameObject ennemie;
+        GameObject fireBall;
 
         public Game1()
         {
@@ -30,9 +34,9 @@ namespace Exercice01
         {
             // TODO: Add your initialization logic here
             this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
-            this.graphics.PreferredBackBufferHeight= graphics.GraphicsDevice.DisplayMode.Height;
+            this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
 
-            this.graphics.ToggleFullScreen();
+            this.graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -50,12 +54,26 @@ namespace Exercice01
             fenetre = graphics.GraphicsDevice.Viewport.Bounds;
             fenetre.Width = graphics.GraphicsDevice.DisplayMode.Width;
             fenetre.Height = graphics.GraphicsDevice.DisplayMode.Height;
-
+            //Chargement du héros
             heros = new GameObject();
             heros.estVivant = true;
             heros.vitesse = 5;
             heros.sprite = Content.Load<Texture2D>("Mario.png");
             heros.position = heros.sprite.Bounds;
+            heros.position.X = fenetre.Right / 2;
+            heros.position.Y = fenetre.Bottom / 2;
+            //Chargement de l'ennemie
+            ennemie = new GameObject();
+            ennemie.estVivant = true;
+            ennemie.vitesse = 5;
+            ennemie.sprite = Content.Load<Texture2D>("EnnemieMonster.png");
+            ennemie.position = ennemie.sprite.Bounds;
+            //Chargement FireBall
+            fireBall = new GameObject();
+            fireBall.estVivant = true;
+            fireBall.vitesse = 5;
+            fireBall.sprite = Content.Load<Texture2D>("FireBall/fireball1.png");
+            fireBall.position = fireBall.sprite.Bounds;
 
         }
 
@@ -75,6 +93,7 @@ namespace Exercice01
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            #region MouvementHeros
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -94,9 +113,25 @@ namespace Exercice01
             {
                 heros.position.Y += heros.vitesse;
             }
-
+            #endregion
+            #region EnnemieMouvement
+            if (isDirection == true)
+            {
+                ennemie.position.X += ennemie.vitesse;
+            }
+            else
+            {
+                ennemie.position.X -= ennemie.vitesse;
+            }
+            #endregion
+            if (Keyboard.GetState().IsKeyDown(Keys.R))
+            {
+                heros.estVivant = true;
+            }
             // TODO: Add your update logic here
             UpdateHeros();
+            UpdateEnnemie();
+            UpdateFireBall();
             base.Update(gameTime);
         }
 
@@ -118,8 +153,38 @@ namespace Exercice01
             {
                 heros.position.Y = fenetre.Bottom - heros.sprite.Bounds.Height;
             }
+            if (heros.position.Intersects(fireBall.position))
+            {
+                heros.estVivant = false;
+            }
         }
-
+        protected void UpdateEnnemie()
+        {
+            if (ennemie.position.X < fenetre.Left)
+            {
+                isDirection = true;
+            }
+            else if(ennemie.position.X + ennemie.sprite.Bounds.Width > fenetre.Right)
+            {
+                isDirection = false;
+            }
+        }
+        protected void UpdateFireBall()
+        {
+            if (fireBall.estVivant == true)
+            {
+                fireBall.position.Y += fireBall.vitesse;
+            }
+            else
+            {
+                fireBall.position = ennemie.position;
+                fireBall.estVivant = true;
+            }
+            if (fireBall.position.Y + fireBall.sprite.Bounds.Height > fenetre.Bottom)
+            {
+                fireBall.estVivant = false;
+            }
+        }
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -131,8 +196,12 @@ namespace Exercice01
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-
-            spriteBatch.Draw(heros.sprite, heros.position, Color.White);
+            if (heros.estVivant == true)
+            {
+                spriteBatch.Draw(heros.sprite, heros.position, Color.White);
+            }
+            spriteBatch.Draw(ennemie.sprite, ennemie.position, Color.White);
+            spriteBatch.Draw(fireBall.sprite, fireBall.position, Color.White);
 
             spriteBatch.End();
             base.Draw(gameTime);
